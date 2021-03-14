f411846c4441b84457bc9d2d898bf2a4dfc2f2e3
alandev2 <alanworking1@gmail.com>
2021-03-14 20:50:12 -0300

    refactor: edit dependencie in SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,10 +2,11 @@ import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest, ok } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
-  constructor (private readonly emailValidator: any) {}
+  constructor (private readonly emailValidator: EmailValidator) {}
 
   handle (httpRequest: HttpRequest): HttpResponse<any> {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
@@ -17,7 +18,7 @@ export class SignUpController implements Controller {
 
     const { email } = httpRequest.body
 
-    if (this.emailValidator.isValid(email) === false) {
+    if (!this.emailValidator.isValid(email)) {
       return badRequest(new InvalidParamError('email'))
     }
 
